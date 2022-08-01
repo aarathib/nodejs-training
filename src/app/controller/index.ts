05de390c1802db4aa156ca120aabd5ed26f04d35
aarathi <aarathy00@gmail.com>
2022-08-01 18:42:42 +0530

    Employee Changes

--- src/app/controller/index.ts
+++ src/app/controller/index.ts
@@ -1,7 +1,11 @@
 /**
  * Wraps Controllers for easy import from other modules
  */
+import { EmployeeRespository } from "../Repository/employeeRepo";
+import { EmployeeService } from "../service/EmployeeService";
+import EmployeeController from "./EmployeeController";
 import HealthController from "./HealthController";
 export default [
   new HealthController(),
+  new EmployeeController(new EmployeeService(new EmployeeRespository()))
 ];
